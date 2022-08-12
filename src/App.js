4c32230ae270d0a0d6c1ef408f73eca64a23751e
ibaslogic <ibaskunle@gmail.com>
2022-08-12 12:25:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import { css, cx } from '@linaria/core';
+import { cx } from '@linaria/core';
+import { css } from '@linaria/atomic';
+import { styled } from '@linaria/react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className={eleStyle}>
+        Hello React <span>world!</span>
+      </h1>
+
+      <p className={cx(class1, class2)}>
+        Using the cx helper function
+      </p>
+
+      <Button primary>click me</Button>
+      <FancyButton>Another button</FancyButton>
+      <FancyButton success>Success</FancyButton>
+    </>
   );
-}
+};
 
 export default App;
+
+// css tagged template
+
+const objStyle = {
+  paddingTop: '3rem',
+};
+
+const eleStyle = css`
+  color: red;
+  font-size: 3rem;
+
+  ${objStyle}
+
+  span {
+    color: green;
+  }
+
+  @media (min-width: 768px) {
+    color: violet;
+  }
+  &:hover {
+    background-color: black;
+  }
+`;
+
+const class1 = css`
+  text-transform: capitalize;
+  background-color: blue;
+`;
+const class2 = css`
+  color: white;
+  background-color: green;
+`;
+
+// end css tagged template
+
+// styled tag begins
+
+const Button = styled.button`
+  /* background-color: blue; */
+  border: none;
+  color: white;
+  padding: 8px 12px;
+  border-radius: 4px;
+  margin-right: 3px;
+  cursor: pointer;
+  background-color: ${(props) =>
+    props.primary ? 'blue' : props.success ? 'green' : 'red'};
+`;
+
+const FancyButton = styled(Button)`
+  /* background-color: black; */
+  text-decoration: underline;
+`;
---
+++ src/styles/globals.js
@@ -0,0 +1,19 @@
+import { css } from '@linaria/core';
+
+export const globals = css`
+  :global() {
+    html {
+      box-sizing: border-box;
+    }
+
+    *,
+    *:before,
+    *:after {
+      box-sizing: inherit;
+    }
+
+    body {
+      background-color: #cecece;
+    }
+  }
+`;
